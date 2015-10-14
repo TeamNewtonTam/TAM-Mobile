9050e9ddc10d443b41ac0ec2a6ecff390870c969
Nick Williamson <ncw@qad.com>
2015-10-14 22:55:29 +0000

    Changes from Screen Builder

---
+++ Hybrid test/components/promoListView/index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+app.promoListView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_promoListView
+// END_CUSTOM_CODE_promoListView
+(function(parent) {
+    var dataProvider = app.data.defaultProvider,
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        // Location type property
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'Activities',
+                dataProvider: dataProvider
+            },
+
+            change: function(e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    flattenLocationProperties(dataItem);
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'Text': {
+                            field: 'Text',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+        },
+        dataSource = new kendo.data.DataSource(dataSourceOptions),
+        promoListViewModel = kendo.observable({
+            dataSource: dataSource
+        });
+
+    parent.set('promoListViewModel', promoListViewModel);
+})(app.promoListView);
+
+// START_CUSTOM_CODE_promoListViewModel
+// END_CUSTOM_CODE_promoListViewModel
